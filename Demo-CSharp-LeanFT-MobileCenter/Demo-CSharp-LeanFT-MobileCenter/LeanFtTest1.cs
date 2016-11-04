4188192d05f1b8b100c85cdcb7ccdf196333aa95
Adam THomas <adamdthomas@gmail.com>
2016-11-04 16:54:37 -0400

    Seperating Mobile Center Demo from LeanFT Only Demo

---
+++ Demo-CSharp-LeanFT-MobileCenter/Demo-CSharp-LeanFT-MobileCenter/LeanFtTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+
+namespace Demo_CSharp_LeanFT_MobileCenter
+{
+    [TestFixture]
+    public class LeanFtTest1 : UnitTestClassBase<LeanFtTest>
+    {
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            // Setup once per fixture
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Before each test
+        }
+
+        [Test]
+        public void TestNunit()
+        {
+            Assert.AreEqual(1, 1);
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
+        {
+            // Clean up once per fixture
+        }
+    }
+}
